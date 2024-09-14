bfb68fae2a7f11f5c562e78a3b72fe7e4e2762f6
TheNoZER0 <hypersonic3692@gmail.com>
2024-09-14 20:25:41 +1000

    Commented thrifted jeans out cause bug

--- algorithm.py
+++ algorithm.py
@@ -83,20 +83,20 @@ class Algorithm():
         
         #######################################################################
         # Other strategies
-        desiredPositions["UQ Dollar"] = self.get_uq_dollar_position(currentPositions["UQ Dollar"], positionLimits["UQ Dollar"])
+        # desiredPositions["UQ Dollar"] = self.get_uq_dollar_position(currentPositions["UQ Dollar"], positionLimits["UQ Dollar"])
         
-        desiredPositions["Thrifted Jeans"] = positionLimits["Thrifted Jeans"]
-        jeans_df = pd.DataFrame(self.data["Thrifted Jeans"])
-        jeans_df['EMA5'] = jeans_df[0].ewm(span=4, adjust=False).mean()
-        # Buy if the price is above the 5 day EMA
-        price = self.data['Thrifted Jeans'][-1]
-        ema = jeans_df['EMA5'].iloc[-1]
-        if price > ema:
-            desiredPositions["Thrifted Jeans"] = -positionLimits["Thrifted Jeans"]
-        else:
-            desiredPositions["Thrifted Jeans"] = positionLimits["Thrifted Jeans"]
+        # desiredPositions["Thrifted Jeans"] = positionLimits["Thrifted Jeans"]
+        # jeans_df = pd.DataFrame(self.data["Thrifted Jeans"])
+        # jeans_df['EMA5'] = jeans_df[0].ewm(span=4, adjust=False).mean()
+        # # Buy if the price is above the 5 day EMA
+        # price = self.data['Thrifted Jeans'][-1]
+        # ema = jeans_df['EMA5'].iloc[-1]
+        # if price > ema:
+        #     desiredPositions["Thrifted Jeans"] = -positionLimits["Thrifted Jeans"]
+        # else:
+        #     desiredPositions["Thrifted Jeans"] = positionLimits["Thrifted Jeans"]
 
-        desiredPositions["Red Pens"] = self.get_red_pens_position(currentPositions["Red Pens"], positionLimits["Red Pens"])
+        # desiredPositions["Red Pens"] = self.get_red_pens_position(currentPositions["Red Pens"], positionLimits["Red Pens"])
 
         #######################################################################
         # Return the desired positions
