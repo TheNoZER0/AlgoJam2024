ba5fd7f45bd0599712c67b63c65c89ee7c5d19ef
jameshurst <james.jude.hurst@gmail.com>
2024-09-14 16:09:53 +1000

    Added stuff to analyse jeans with an EMA

--- Data Analysis/Thrifted Jeans Analysis.py
+++ Data Analysis/Thrifted Jeans Analysis.py
@@ -4,3 +4,5 @@ import pandas as pd
 # Load Thrifted Jeans data
 jeans = pd.read_csv('Data Analysis/Thrifted Jeans Data.csv')
 
+# plot the data
+jeans.plot.scatter(x='Price', y='Resale Price', title='Resale Price vs. Price')
